ce390d0b03a4990e2082263f881b53c73b8785d8
Kangil Lee <klee0589@gmail.com>
2020-01-09 12:28:00 -0500

    Playground (#1)
    * theme migration
    
    * setting up theme UI intake
    
    * better setup
    
    * transfered logo into theme
    
    * organize theme folder layout
    
    * include vue-router

---
+++ levantr/src/components/Calendar.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="app-content content">
+    <div class="content-overlay"></div>
+    <div class="content-wrapper">
+      <div class="content-header row"></div>
+      <div class="content-body">
+        <!-- calendar Wrapper  -->
+        <div class="calendar-wrapper position-relative">
+          <!-- calendar app overlay -->
+          <div class="app-content-overlay"></div>
+          <!-- calendar sidebar start -->
+          <div id="sidebar" class="sidebar">
+            <div class="sidebar-new-schedule">
+              <!-- create new schedule button -->
+              <button
+                id="btn-new-schedule"
+                type="button"
+                class="btn btn-primary btn-block sidebar-new-schedule-btn"
+              >New schedule</button>
+            </div>
+            <!-- sidebar calendar labels -->
+            <div id="sidebar-calendars" class="sidebar-calendars">
+              <div>
+                <div class="sidebar-calendars-item">
+                  <!-- view All checkbox -->
+                  <div class="checkbox">
+                    <input
+                      type="checkbox"
+                      class="checkbox-input tui-full-calendar-checkbox-square"
+                      id="checkbox1"
+                      value="all"
+                      checked
+                    />
+                    <label for="checkbox1">View all</label>
+                  </div>
+                </div>
+              </div>
+              <div id="calendarList" class="sidebar-calendars-d1"></div>
+            </div>
+            <!-- / sidebar calendar labels -->
+          </div>
+          <!-- calendar sidebar end -->
+          <!-- calendar view start  -->
+          <div class="calendar-view">
+            <div class="calendar-action d-flex align-items-center flex-wrap">
+              <!-- sidebar toggle button for small sceen -->
+              <button class="btn btn-icon sidebar-toggle-btn">
+                <i class="bx bx-menu font-large-1"></i>
+              </button>
+              <!-- dropdown button to change calendar-view -->
+              <div class="dropdown d-inline mr-75">
+                <button
+                  id="dropdownMenu-calendarType"
+                  class="btn btn-action dropdown-toggle"
+                  type="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="true"
+                >
+                  <i id="calendarTypeIcon" class="bx bx-calendar-alt"></i>
+                  <span id="calendarTypeName">Dropdown</span>
+                </button>
+                <ul
+                  class="dropdown-menu dropdown-menu-right"
+                  role="menu"
+                  aria-labelledby="dropdownMenu-calendarType"
+                >
+                  <li role="presentation">
+                    <a
+                      class="dropdown-menu-title dropdown-item"
+                      role="menuitem"
+                      data-action="toggle-daily"
+                    >
+                      <i class="bx bx-calendar-alt mr-50"></i>
+                      <span>Daily</span>
+                    </a>
+                  </li>
+                  <li role="presentation">
+                    <a
+                      class="dropdown-menu-title dropdown-item"
+                      role="menuitem"
+                      data-action="toggle-weekly"
+                    >
+                      <i class="bx bx-calendar-event mr-50"></i>
+                      <span>Weekly</span>
+                    </a>
+                  </li>
+                  <li role="presentation">
+                    <a
+                      class="dropdown-menu-title dropdown-item"
+                      role="menuitem"
+                      data-action="toggle-monthly"
+                    >
+                      <i class="bx bx-calendar mr-50"></i>
+                      <span>Month</span>
+                    </a>
+                  </li>
+                  <li role="presentation">
+                    <a
+                      class="dropdown-menu-title dropdown-item"
+                      role="menuitem"
+                      data-action="toggle-weeks2"
+                    >
+                      <i class="bx bx-calendar-check mr-50"></i>
+                      <span>2 weeks</span>
+                    </a>
+                  </li>
+                  <li role="presentation">
+                    <a
+                      class="dropdown-menu-title dropdown-item"
+                      role="menuitem"
+                      data-action="toggle-weeks3"
+                    >
+                      <i class="bx bx-calendar-check mr-50"></i>
+                      <span>3 weeks</span>
+                    </a>
+                  </li>
+                  <li role="presentation" class="dropdown-divider"></li>
+                  <li role="presentation">
+                    <div role="menuitem" data-action="toggle-workweek" class="dropdown-item">
+                      <input
+                        type="checkbox"
+                        class="tui-full-calendar-checkbox-square"
+                        value="toggle-workweek"
+                        checked
+                      />
+                      <span class="checkbox-title bg-primary"></span>
+                      <span>Show weekends</span>
+                    </div>
+                  </li>
+                  <li role="presentation">
+                    <div role="menuitem" data-action="toggle-start-day-1" class="dropdown-item">
+                      <input
+                        type="checkbox"
+                        class="tui-full-calendar-checkbox-square"
+                        value="toggle-start-day-1"
+                      />
+                      <span class="checkbox-title"></span>
+                      <span>Start Week on Monday</span>
+                    </div>
+                  </li>
+                  <li role="presentation">
+                    <div role="menuitem" data-action="toggle-narrow-weekend" class="dropdown-item">
+                      <input
+                        type="checkbox"
+                        class="tui-full-calendar-checkbox-square"
+                        value="toggle-narrow-weekend"
+                      />
+                      <span class="checkbox-title"></span>
+                      <span>Narrower than weekdays</span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <!-- calenadar next and previous navigate button -->
+              <span id="menu-navi" class="menu-navigation">
+                <button
+                  type="button"
+                  class="btn btn-action move-today mr-50 px-75"
+                  data-action="move-today"
+                >Today</button>
+                <button
+                  type="button"
+                  class="btn btn-icon btn-action move-day mr-50 px-50"
+                  data-action="move-prev"
+                >
+                  <i class="bx bx-chevron-left" data-action="move-prev"></i>
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-icon btn-action move-day mr-50 px-50"
+                  data-action="move-next"
+                >
+                  <i class="bx bx-chevron-right" data-action="move-next"></i>
+                </button>
+              </span>
+              <span id="renderRange" class="render-range"></span>
+            </div>
+            <!-- calendar view  -->
+            <div id="calendar" class="calendar-content"></div>
+          </div>
+          <!-- calendar view end  -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeMount() {
+    let ckeditor = document.createElement("script");
+    ckeditor.setAttribute("src", "./app-assets/js/scripts/extensions/calendar/schedules.js");
+    document.head.appendChild(ckeditor);
+  },
+  mounted() {
+    let ckeditor = document.createElement("script");
+    ckeditor.setAttribute("src", "./app-assets/js/scripts/extensions/calendar/app-calendar.js");
+    document.head.appendChild(ckeditor);
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ levantr/src/components/Header.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="header-navbar-shadow"></div>
+    <nav
+      class="header-navbar main-header-navbar navbar-expand-lg navbar navbar-with-menu fixed-top"
+    >
+      <div class="navbar-wrapper">
+        <div class="navbar-container content">
+          <div class="navbar-collapse" id="navbar-mobile">
+            <div
+              class="mr-auto float-left bookmark-wrapper d-flex align-items-center"
+            >
+              <ul class="nav navbar-nav">
+                <li class="nav-item mobile-menu d-xl-none mr-auto">
+                  <a
+                    class="nav-link nav-menu-main menu-toggle hidden-xs"
+                    href="#"
+                  >
+                    <i class="ficon bx bx-menu"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <ul class="nav navbar-nav float-right">
+              <li class="dropdown dropdown-user nav-item">
+                <a
+                  class="dropdown-toggle nav-link dropdown-user-link"
+                  href="#"
+                  data-toggle="dropdown"
+                >
+                  <div class="user-nav d-sm-flex d-none">
+                    <span class="user-name">John Doe</span>
+                    <!-- <span class="user-status text-muted">Available</span> -->
+                  </div>
+                  <span>
+                    <img
+                      class="round"
+                      src="../../public/app-assets/images/portrait/small/avatar-s-11.jpg"
+                      alt="avatar"
+                      height="40"
+                      width="40"
+                    />
+                  </span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right pb-0">
+                  <a class="dropdown-item" href="page-user-profile.html">
+                    <i class="bx bx-user mr-50"></i> Edit Profile
+                  </a>
+                  <a class="dropdown-item" href="app-email.html">
+                    <i class="bx bx-envelope mr-50"></i> My Inbox
+                  </a>
+                  <a class="dropdown-item" href="app-todo.html">
+                    <i class="bx bx-check-square mr-50"></i> Task
+                  </a>
+                  <a class="dropdown-item" href="app-chat.html">
+                    <i class="bx bx-message mr-50"></i> Chats
+                  </a>
+                  <div class="dropdown-divider mb-0"></div>
+                  <a class="dropdown-item" href="auth-login.html">
+                    <i class="bx bx-power-off mr-50"></i> Logout
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
